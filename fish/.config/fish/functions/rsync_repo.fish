3484b3ff9a9a420683a2e6197eb0e2e287b8225c
axionl <i@axionl.me>
2019-01-18 23:23:12 +0900

    [Update] fish: change the rsync_repo function; fcitx5: update config

--- fish/.config/fish/functions/rsync_repo.fish
+++ fish/.config/fish/functions/rsync_repo.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function rsync_repo --description 'alias rsync_repo rsync ~/Programs/archlinux/local/axionl_repo/* -avzr --progress --password-file=/home/axionl/.ssh/others/rsync.pwd http@45.77.6.73::repo'
-	rsync ~/Programs/archlinux/local/axionl_repo/* -avzr --progress --password-file=/home/axionl/.ssh/others/rsync.pwd http@45.77.6.73::repo $argv;
+	rsync ~/Programs/archlinux/local/axionl_repo/* -avzr --progress --password-file=/home/axionl/.ssh/others/rsync.pwd html@45.77.6.73::repo $argv;
 end
