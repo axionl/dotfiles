0bae181735b8086b8002021bb23a192d6f843ff0
axionl <i@axionl.me>
2019-04-17 22:21:50 +0800

    [Update] fish: change the default range to 25 of pacsize

--- fish/.config/fish/functions/pacsize.fish
+++ fish/.config/fish/functions/pacsize.fish
@@ -1,4 +1,8 @@
 # Defined in - @ line 0
-function pacsize --description 'Order packages by size'
-	pacman -Qi | awk '/^Name/{name=$3} /^Installed Size/{print $4$5, name}' | sort -h -r | bat -r :$argv;
+function pacsize --description 'Order packages by size (Default: 25)'
+    if count $argv > /dev/null
+	    pacman -Qi | awk '/^Name/{name=$3} /^Installed Size/{print $4$5, name}' | sort -h -r | bat -r :$argv;
+    else
+        pacman -Qi | awk '/^Name/{name=$3} /^Installed Size/{print $4$5, name}' | sort -h -r | bat -r :25;
+    end
 end
