349e013f70d1ae46c3ca57ef7f38bd584c22a875
axionl <i@axionl.me>
2019-04-30 20:09:49 +0800

    [Update] add alias for translate-shell

---
+++ fish/.config/fish/functions/en.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function en --description 'trans :en'
+	trans :en $argv;
+end
---
+++ fish/.config/fish/functions/zh.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function zh --description 'trans :zh'
+	trans :zh $argv;
+end
